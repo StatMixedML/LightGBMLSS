bb282c189b3374d15b2d3fb4e1276d053748a36e
Alexander März <alex.maerz@gmx.net>
2022-01-05 16:50:39 +0100

    Update __init__.py

--- lightgbmlss/distributions/__init__.py
+++ lightgbmlss/distributions/__init__.py
@@ -2,4 +2,9 @@
 
 from lightgbmlss.distributions.Gaussian import *
 from lightgbmlss.distributions.Gaussian_AutoGrad import *
+from lightgbmlss.distributions.StudentT import *
+from lightgbmlss.distributions.Gamma import *
+from lightgbmlss.distributions.Beta import *
+from lightgbmlss.distributions.NegativeBinomial import *
+from lightgbmlss.distributions.BCT import *
 from lightgbmlss.distributions.Expectile import *
