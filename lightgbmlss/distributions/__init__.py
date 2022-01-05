54addef94ba1addee1d4346abf40e142ef5337a0
Alexander März <alex.maerz@gmx.net>
2022-01-05 10:14:58 +0100

    Update __init__.py

--- lightgbmlss/distributions/__init__.py
+++ lightgbmlss/distributions/__init__.py
@@ -1,3 +1,5 @@
 """LightGBMLSS - An extension of LightGBM to probabilistic forecasting"""
 
 from lightgbmlss.distributions.Gaussian import *
+from lightgbmlss.distributions.Gaussian_AutoGrad import *
+from lightgbmlss.distributions.Expectile import *
