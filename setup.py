f251b5b7669498833b5086f29c1dfad5c6a9cc09
Alexander März <alex.maerz@gmx.net>
2023-05-26 18:26:04 +0200

    Update to v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lightgbmlss",
-    version="0.2.0",
+    version="0.2.1",
     description="LightGBMLSS - An extension of LightGBM to probabilistic forecasting",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
